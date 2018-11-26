9590d71d7266a7f1fbf9e9c642555d13c559ac21
Soheil <Soheil.Tajahmadi@gmail.com>
2018-11-26 17:03:11 +1100

    Updates

---
+++ precache-manifest.48106b29fae8dcbb0e60e85744c1cac5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1affb07ec8006774800c",
+    "url": "/lease_payments/static/js/runtime~main.1affb07e.js"
+  },
+  {
+    "revision": "d0b0f09da3aebb4f434f",
+    "url": "/lease_payments/static/js/main.d0b0f09d.chunk.js"
+  },
+  {
+    "revision": "0a31924ff03aabbb71df",
+    "url": "/lease_payments/static/js/1.0a31924f.chunk.js"
+  },
+  {
+    "revision": "d0b0f09da3aebb4f434f",
+    "url": "/lease_payments/static/css/main.c15f19f7.chunk.css"
+  },
+  {
+    "revision": "4e744568e2255cd2ea17b037ecd728a1",
+    "url": "/lease_payments/index.html"
+  }
+];
